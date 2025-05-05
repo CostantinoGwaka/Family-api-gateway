ec05064cd50267cd6ba4f670fdc7711c8b3d0946
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-05-05 15:38:36 +0300

    init

---
+++ src/main/java/com/isofttz/family/gateway/family_gateway/FamilyGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.isofttz.family.gateway.family_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyGatewayApplication.class, args);
+	}
+
+}
